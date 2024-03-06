9701e53acb8311fb6099e499ae2c826537c860fa
Kinjal Das <thekdas1000@gmail.com>
2024-03-06 10:23:29 +0530

    initialised

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-// express 4 package
 const express = require('express');
 const app = express();
 
-// res.send to send the string data 
-
-// GET request is the result we get from the HTTP request
+// GET -> what we get from the browser
 app.get('/', (req, res) => {
-    res.status(200).json(
-        {
-            message: 'Hello from the server side',
-            app: 'natours'
-        }
-    );
+  res.status(200).json({
+    message: 'Hello from the other side',
+    app: 'natours'
+  })
 });
 
-
-// POST request is what we send to the browser
+// POST -> sent to the browser
 app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
-});
-
+  res.send('You can post on this endpoint...');
+})
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+  console.log(`Listening on port ${port}`);
+})
