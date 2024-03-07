f660a81c23bb5fb248e0ddb7181ce9932615d2a0
Kinjal Das <thekdas1000@gmail.com>
2024-03-07 09:30:29 +0530

    GET and POST testing done

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 // GET -> what we get from the browser
-app.get('/', (req, res) => {
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the other side',
-    app: 'natours'
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours
+    }
   })
 });
 
-// POST -> sent to the browser
-app.post('/', (req, res) => {
-  res.send('You can post on this endpoint...');
-})
+// // POST -> sent to the browser
+app.post('/api/v1/tours', (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  // copy the req.body enumerables to the object having id: newID
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour
+      }
+    })
+  })
+});
+
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}...`);
 })
